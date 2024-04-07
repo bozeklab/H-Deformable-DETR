4a2c3b27d65b05d721751f8b03a236a66b9e50a2
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-07 10:17:41 +0200

    save evaluator

--- engine.py
+++ engine.py
@@ -237,7 +237,7 @@ def evaluate(
             for target, output in zip(targets, results)
         }
         print('!!!!')
-        print(samples)
+        print(samples.shape)
         print(results[0]['boxes'])
         if coco_evaluator is not None:
             coco_evaluator.update(res)
