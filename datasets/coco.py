190c669a953a9eda085935dcf2619433c9fe0c8e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-27 22:32:14 +0200

    debug

--- datasets/coco.py
+++ datasets/coco.py
@@ -148,19 +148,19 @@ def make_coco_transforms(image_set):
         [T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
     )
 
-    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    scales1 = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 
     if image_set == "train":
         return T.Compose(
             [
                 T.RandomHorizontalFlip(),
                 T.RandomSelect(
-                    T.RandomResize(scales, max_size=1333),
+                    T.RandomResize(scales1, max_size=1333),
                     T.Compose(
                         [
                             T.RandomResize([400, 500, 600]),
                             T.RandomSizeCrop(384, 600),
-                            T.RandomResize(scales, max_size=1333),
+                            T.RandomResize(scales1, max_size=1333),
                         ]
                     ),
                 ),
