cda0c5f09f054063ada688c6e7f461a286e6f731
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-29 16:25:10 +0200

    debug

--- main.py
+++ main.py
@@ -351,6 +351,7 @@ def main(args):
     ]
     print('!!!')
     print(param_dicts[1].keys())
+    print(param_dicts[2].keys())
     if args.sgd:
         optimizer = torch.optim.SGD(
             param_dicts, lr=args.lr, momentum=0.9, weight_decay=args.weight_decay
