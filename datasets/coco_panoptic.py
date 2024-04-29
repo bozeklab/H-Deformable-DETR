986970ff03a4e77514de2c4e747269c48bbe29cf
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-29 10:50:57 +0200

    debug

--- datasets/coco_panoptic.py
+++ datasets/coco_panoptic.py
@@ -94,6 +94,7 @@ def build(image_set, args):
     PATHS = {
         "train": ("train2017", Path("annotations") / f'{mode}_train2017.json'),
         "val": ("val2017", Path("annotations") / f'{mode}_val2017.json'),
+        "test": ("test2017", Path("annotations") / f'{mode}_test2017.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
