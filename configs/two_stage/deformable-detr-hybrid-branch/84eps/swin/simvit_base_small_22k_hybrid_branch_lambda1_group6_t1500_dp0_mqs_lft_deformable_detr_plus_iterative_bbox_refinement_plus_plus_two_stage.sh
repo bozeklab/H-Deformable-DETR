75e7aec8bd2bddb72effdcf35a9b19e1b4c6f94e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-05 00:01:49 +0200

    augmentations

--- configs/two_stage/deformable-detr-hybrid-branch/84eps/swin/simvit_base_small_22k_hybrid_branch_lambda1_group6_t1500_dp0_mqs_lft_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh
+++ configs/two_stage/deformable-detr-hybrid-branch/84eps/swin/simvit_base_small_22k_hybrid_branch_lambda1_group6_t1500_dp0_mqs_lft_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh
@@ -2,16 +2,17 @@
 
 set -x
 
-EXP_DIR=exps/two_stage/deformable-detr-hybrid-branch/84eps/swin/swin_tiny_hybrid_branch_lambda1_group6_t1500_dp0_mqs_lft_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage
+EXP_DIR=exps/two_stage/deformable-detr-hybrid-branch/72eps/swin/simvit_base_22k_hybrid_branch_lambda1_group6_t1500_dp0_mqs_lft_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage
 PY_ARGS=${@:1}
 
 python -u main.py \
     --output_dir ${EXP_DIR} \
     --with_box_refine \
     --two_stage \
+    --lr_backbone_names body.backbone \
     --dim_feedforward 2048 \
     --epochs 84 \
-    --lr_drop 30 \
+    --lr_drop 72 \
     --num_queries_one2one 300 \
     --num_queries_one2many 1500 \
     --k_one2many 6 \
@@ -19,6 +20,6 @@ python -u main.py \
     --dropout 0.0 \
     --mixed_selection \
     --look_forward_twice \
-    --backbone swin_tiny \
-    --pretrained_backbone_path pretrained_backbone/swin_tiny_patch4_window7_224.pth \
+    --backbone simvit_base \
+    --pretrained_backbone_path /data/pwojcik/SimMIM/TCGA_256/checkpoint-latest.pth \
     ${PY_ARGS}
