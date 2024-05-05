0506b890f5e219a98588fa64e86c9c1638bf16ca
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-05 11:52:52 +0200

    augmentations

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -91,14 +91,13 @@ def vflip(image, target):
     target = target.copy()
     if "boxes" in target:
         boxes = target["boxes"]
+        print(boxes)
         boxes = boxes[:, [2, 1, 0, 3]] * torch.as_tensor(
             [1, -1, 1, -1]
         ) + torch.as_tensor([0, h, 0, h])
-        target["boxes"] = boxes
+        #target["boxes"] = boxes
 
     if "masks" in target:
-        print('!!!')
-        print('dupa')
         target["masks"] = target["masks"].flip(-2)
 
     return flipped_image, target
