8efae127f92f68bbce281bbfbdb18ed8a8e50c98
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-05 22:27:06 +0200

    augmentations

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -146,7 +146,7 @@ class DeformableDETR(nn.Module):
             # hack implementation for iterative bounding box refinement
             self.transformer.decoder.bbox_embed = self.bbox_embed
         else:
-            print('!!!')
+            print('!!refine!')
             nn.init.constant_(self.bbox_embed.layers[-1].bias.data[2:], -2.0)
             self.class_embed = nn.ModuleList(
                 [self.class_embed for _ in range(num_pred)]
