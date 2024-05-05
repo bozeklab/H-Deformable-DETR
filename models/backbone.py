c7dc7b8718e51abb988398f78c5cd5e1c7cee3e4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-05 21:41:44 +0200

    augmentations

--- models/backbone.py
+++ models/backbone.py
@@ -301,9 +301,6 @@ def build_backbone(args):
     position_embedding = build_position_encoding(args)
     train_backbone = args.lr_backbone > 0
     return_interm_layers = args.masks or (args.num_feature_levels > 1)
-    print('!!!')
-    print(args.masks)
-    print(args.num_feature_levels)
     if "resnet" in args.backbone:
         backbone = Backbone(
             args.backbone, train_backbone, return_interm_layers, args.dilation,
