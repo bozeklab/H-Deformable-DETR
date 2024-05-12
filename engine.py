88a9b55478a3366f8d9086065f04352949b68bba
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-12 21:07:28 +0200

    swin config

--- engine.py
+++ engine.py
@@ -178,10 +178,10 @@ def train_one_epoch(
 def predict_prompts(prompts_paths, dataset_name, model, postprocessors):
     mkdir(f'../segmentor/{prompts_paths}')
     print('Test files')
-    test_files = np.load(f'../segmentor/datasets/{dataset_name}_test_files.npy')
+    test_files = np.load(f'/data/pwojcik/PromptNucSeg/segmentor/datasets/{dataset_name}_test_files.npy')
     process_files(test_files, model, postprocessors)
     print('Val files')
-    val_files = np.load(f'../segmentor/datasets/{dataset_name}_val_files.npy')
+    val_files = np.load(f'/data/pwojcik/PromptNucSeg/segmentor/datasets/{dataset_name}_val_files.npy')
     process_files(val_files, model, postprocessors)
 
 
