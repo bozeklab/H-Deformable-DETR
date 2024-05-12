0eb83c0cc6aeea1ced88f522ab50ae07c93e87c6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-12 21:35:06 +0200

    swin config

--- engine.py
+++ engine.py
@@ -179,7 +179,6 @@ def predict_prompts(prompts_paths, dataset_name, model, postprocessors):
     mkdir(f'../segmentor/{prompts_paths}')
     print('Test files')
     test_files = np.load(f'/data/pwojcik/PromptNucSeg/segmentor/datasets/{dataset_name}_test_files.npy')
-    print(test_files)
     process_files(test_files, model, postprocessors)
     print('Val files')
     val_files = np.load(f'/data/pwojcik/PromptNucSeg/segmentor/datasets/{dataset_name}_val_files.npy')
@@ -188,7 +187,7 @@ def predict_prompts(prompts_paths, dataset_name, model, postprocessors):
 
 def process_files(files, model, postprocessors):
     for file in sorted(tqdm(files)):
-        img = io.imread(f'../segmentor/{file}')[..., :3]
+        img = io.imread(f'/data/pwojcik/PromptNucSeg/segmentor/{file}')[..., :3]
 
         normalize = T.Compose(
             [T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
