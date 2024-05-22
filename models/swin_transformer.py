7d7689e5937c4e5fa6ed7b78457d058648f5acfc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-22 13:03:50 +0200

    swin config

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -706,8 +706,6 @@ class SwinTransformer(nn.Module):
 
     def forward(self, x):
         """Forward function."""
-        print('trans')
-        print(x.shape)
         #assert(x.shape[2] == x.shape[3])
         x = self.patch_embed(x)
         Wh, Ww = x.size(2), x.size(3)
