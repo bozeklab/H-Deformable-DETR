feea65826912d21e79a667dbb89f62b4f5d0fe60
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-23 16:51:49 +0200

    swin config

--- datasets/coco.py
+++ datasets/coco.py
@@ -144,6 +144,9 @@ class ConvertCocoPolysToMask(object):
 
 def make_coco_transforms(image_set):
 
+    print('!!!')
+    print(image_set)
+
     normalize = T.Compose(
         [T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
     )
