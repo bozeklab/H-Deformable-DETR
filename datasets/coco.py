785d0e56ce084ac8dbab19ae90f6022f92e3a87d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-27 12:36:31 +0200

    swin config

--- datasets/coco.py
+++ datasets/coco.py
@@ -148,7 +148,7 @@ def make_coco_transforms(image_set):
         [T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
     )
 
-    #scales = [(scale, scale) for scale in [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]]
+    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 
     if image_set == "train":
         return T.Compose(
@@ -156,28 +156,27 @@ def make_coco_transforms(image_set):
                 T.RandomHorizontalFlip(),
                 T.RandomVerticalFlip(),
                 T.GaussianBlur(),
-                #T.Solarize(),
+                # T.Solarize(),
                 T.RandomGrayscale(),
-                #T.RandomHEStain(),
+                # T.RandomHEStain(),
                 T.ColorJitter(),
                 T.GaussianBlur(),
                 T.RandomSelect(
+                    T.RandomResize(scales, max_size=1333),
                     T.Compose(
                         [
-                            #T.RandomResize([(400, 400), (500, 500), (600, 600)]),
-                            T.RandomSizeCrop(100, 256),
-                            T.RandomResize([(256, 256)]),
+                            T.RandomResize([400, 500, 600]),
+                            T.RandomSizeCrop(384, 600),
+                            T.RandomResize(scales, max_size=1333),
                         ]
                     ),
-                    T.RandomResize([(256, 256)]),
-                    p=0.4
                 ),
                 normalize,
             ]
         )
 
-    if image_set == "val" or image_set == "test":
-        return T.Compose([T.RandomResize([256], max_size=1333), normalize])
+    if image_set == "val":
+        return T.Compose([T.RandomResize([800], max_size=1333), normalize,])
 
     raise ValueError(f"unknown {image_set}")
 
@@ -189,7 +188,6 @@ def build(image_set, args, eval_in_training_set):
     PATHS = {
         "train": (root / "train2017", root / "annotations" / f"{mode}_train2017.json"),
         "val": (root / "val2017", root / "annotations" / f"{mode}_val2017.json"),
-        "test": (root / "test2017", root / "annotations" / f"{mode}_test2017.json"),
     }
 
     img_folder, ann_file = PATHS[image_set]
